27953512a610e4caf79b403f3842f82326f5860f
SJXWJK <1298279769@qq.com>
2022-02-20 21:46:19 +0800

    add photos

--- ins.js
+++ ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/sjxwjk/zzblogpicture/master/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/sjxwjk/zzblogpicture/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/sjxwjk/zzblogpicture/main/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/SJXWJK/zzblogpicture/main/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
