372fb2c2ea5d82c787b41ebae31734ffea93187a
SJXWJK <1298279769@qq.com>
2022-02-20 22:44:07 +0800

    1

--- tool.py
+++ tool.py
@@ -1,195 +1,145 @@
-#coding: utf-8
-from PIL import Image
-import os
-import sys
-import json
-from datetime import datetime
-from ImageProcess import Graphics
+#post-instagram{
+    padding: 30px;
+}
+#post-instagram .article-entry{
+    padding-right: 0;
+}
+.instagram{
+    position: relative;
+    min-height: 500px;
+}
+.instagram img {
+    width: 100%;
+}
+.instagram .year {
+    font-size: 16px;
+}
+.instagram .open-ins{
+    padding: 10px 0;
+    color: #cdcdcd;
+}
+.instagram .open-ins:hover{
+    color: #657b83;
+}
+.instagram .year{
+    display: inline;
+}
+.instagram .thumb {
+    width: 25%;
+    height: 0; 
+    padding-bottom: 25%;
+    position: relative;
+    display: inline-block;
+    text-align: center;
+    background: #ededed;
+    outline: 1px solid #ddd;
+}
+.instagram .thumb a {
+    position: relative;
+}
+.instagram .album h1 em{
+    font-style: normal;
+    font-size: 14px;
+    margin-left: 10px;
+}
+.instagram .album ul{
+    display: flex;
+    flex-wrap: wrap;
+    clear: both;
+    width: 100%;
+    text-align: left;
+}
+.instagram .album li{
+    list-style: none;
+    display: inline-block;
+    box-sizing: border-box;
+    padding: 0 5px;
+    margin-bottom: -10px;
+    height: 0;
+    width: 25%;
+    position: relative;
+    padding-bottom: 25%;
+}
+.instagram .album li:before{
+    display: none;
+}
+.instagram .album div.img-box{
+    position: absolute;
+    width: 90%;
+    height: 90%;
+    -webkit-box-shadow: 0 1px 0 rgba(255,255,255,0.4), 0 1px 0 1px rgba(255,255,255,0.1);
+    -moz-box-shadow: 0 1px 0 rgba(255,255,255,0.4), 0 1px 0 1px rgba(255,255,255,0.1);
+    box-shadow: 0 1px 0 rgba(255,255,255,0.4), 0 1px 0 1px rgba(255,255,255,0.1);
+}
+.instagram .album div.img-box img{
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 2;
+}
+.instagram .album div.img-box .img-bg{
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0px;
+    width: 100%;
+    margin: -5px;
+    padding: 5px;
+    -webkit-box-shadow: 0 0 0 1px rgba(0,0,0,.04), 0 1px 5px rgba(0,0,0,0.1);
+    -moz-box-shadow: 0 0 0 1px rgba(0,0,0,.04), 0 1px 5px rgba(0,0,0,0.1);
+    box-shadow: 0 0 0 1px rgba(0,0,0,.04), 0 1px 5px rgba(0,0,0,0.1);
+    -webkit-transition: all 0.15s ease-out 0.1s;
+    -moz-transition: all 0.15s ease-out 0.1s;
+    -o-transition: all 0.15s ease-out 0.1s;
+    transition: all 0.15s ease-out 0.1s;
+    opacity: 0.2;
+    cursor: pointer;
+    display: block;
+    z-index: 3;
+}
+.instagram .album div.img-box .icon {
+    font-size: 14px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    margin-left: -7px;
+    margin-top: -7px;
+    color: #999;
+    z-index: 1;
+}
+.instagram .album div.img-box .img-bg:hover{
+    opacity: 0;
+}
+.photos-btn-wrap {
+    border-bottom: 1px solid #e5e5e5;
+    margin-bottom: 20px;
+}
+.photos-btn {
+    font-size: 16px;
+    color: #333;
+    margin-bottom: -4px;
+    padding: 5px 8px 3px;
+}
+.photos-btn.active {
+    color: #08c;
+    border: 1px solid #e5e5e5;
+    border-bottom: 5px solid #fff;
+}
 
-# 定义压缩比，数值越大，压缩越小
-SIZE_normal = 1.0
-SIZE_small = 1.5
-SIZE_more_small = 2.0
-SIZE_more_small_small = 3.0
-
-
-def make_directory(directory):
-    """创建目录"""
-    os.makedirs(directory)
-
-def directory_exists(directory):
-    """判断目录是否存在"""
-    if os.path.exists(directory):
-        return True
-    else:
-        return False
-
-def list_img_file(directory):
-    """列出目录下所有文件，并筛选出图片文件列表返回"""
-    old_list = os.listdir(directory)
-    # print old_list
-    new_list = []
-    for filename in old_list:
-        name, fileformat = filename.split(".")
-        if fileformat.lower() == "jpg" or fileformat.lower() == "png" or fileformat.lower() == "gif":
-            new_list.append(filename)
-    # print new_list
-    return new_list
-
-
-def print_help():
-    print("""
-    This program helps compress many image files
-    you can choose which scale you want to compress your img(jpg/png/etc)
-    1) normal compress(4M to 1M around)
-    2) small compress(4M to 500K around)
-    3) smaller compress(4M to 300K around)
-    """)
-
-def compress(choose, des_dir, src_dir, file_list):
-    """压缩算法，img.thumbnail对图片进行压缩，
-    
-    参数
-    -----------
-    choose: str
-            选择压缩的比例，有4个选项，越大压缩后的图片越小
-    """
-    if choose == '1':
-        scale = SIZE_normal
-    if choose == '2':
-        scale = SIZE_small
-    if choose == '3':
-        scale = SIZE_more_small
-    if choose == '4':
-        scale = SIZE_more_small_small
-    for infile in file_list:
-        img = Image.open(src_dir+infile)
-        # size_of_file = os.path.getsize(infile)
-        w, h = img.size
-        img.thumbnail((int(w/scale), int(h/scale)))
-        img.save(des_dir + infile)
-def compress_photo():
-    '''调用压缩图片的函数
-    '''
-    src_dir, des_dir = "photos/", "min_photos/"
-    
-    if directory_exists(src_dir):
-        if not directory_exists(src_dir):
-            make_directory(src_dir)
-        # business logic
-        file_list_src = list_img_file(src_dir)
-    if directory_exists(des_dir):
-        if not directory_exists(des_dir):
-            make_directory(des_dir)
-        file_list_des = list_img_file(des_dir)
-        # print file_list
-    '''如果已经压缩了，就不再压缩'''
-    for i in range(len(file_list_des)):
-        if file_list_des[i] in file_list_src:
-            file_list_src.remove(file_list_des[i])
-    compress('4', des_dir, src_dir, file_list_src)
-
-
-def handle_photo():
-    '''根据图片的文件名处理成需要的json格式的数据
-    -----------
-    最后将data.json文件存到博客的source/photos文件夹下
-    '''
-    src_dir, des_dir = "photos/", "min_photos/"
-    file_list = list_img_file(src_dir)
-    list_info = []
-    date_list = []
-    for i in range(len(file_list)):
-        filename = file_list[i]
-        date_str, info = filename.split("_")
-        info, _ = info.split(".")
-        date = datetime.strptime(date_str, "%Y-%m-%d")
-        year_month = '-'.join(date_str.split('-')[0:2])
-        if i == 0:  # 处理第一个文件
-            new_dict = {"date": year_month, "arr": {'year': date.year,
-                                                    'month': date.month,
-                                                    'link': [filename],
-                                                    'text': [info],
-                                                    'type': ['image']
-                                                    }
-                        }
-            date_list.append(date)
-            list_info.append(new_dict)
-        elif date in date_list:  # 是同一个日期,找到在 Date 数组中的索引
-            index = date_list.index(date)
-            list_info[index]['arr']['link'].append(filename)
-            list_info[index]['arr']['text'].append(info)
-            list_info[index]['arr']['type'].append('image')
-            
-        else:  # 不是同一个日期创建一个新的 dict
-            new_dict = {"date": year_month, "arr":{'year': date.year,
-                                                   'month': date.month,
-                                                   'link': [filename],
-                                                   'text': [info],
-                                                   'type': ['image']
-                                                   }
-                        }
-            date_list.append(date)
-            list_info.append(new_dict)
-
-    list_info = SortDict(list_info)
-    final_dict = {"list": list_info}
-    with open("/Users/zz/zzblog/zzblogpicture/photos/data.json", "w") as fp:
-        json.dump(final_dict, fp)
-
-
-# 冒泡排序
-def SortDict(list_info):
-    for num in range(len(list_info)-1,0,-1):
-        for i in range(num):
-            date1 = datetime.strptime(list_info[i]['date'],"%Y-%m")
-            date2 = datetime.strptime(list_info[i+1]['date'],"%Y-%m")
-            if date1 < date2:
-                temp = list_info[i]
-                list_info[i] = list_info[i+1]
-                list_info[i+1] = temp
-    return list_info
-
-
-
-def cut_photo():
-    """裁剪算法
-    
-    ----------
-    调用Graphics类中的裁剪算法，将src_dir目录下的文件进行裁剪（裁剪成正方形）
-    """
-    src_dir = "photos/"
-    if directory_exists(src_dir):
-        if not directory_exists(src_dir):
-            make_directory(src_dir)
-        # business logic
-        file_list = list_img_file(src_dir)
-        # print file_list
-        if file_list:
-            print_help()
-            for infile in file_list:
-                img = Image.open(src_dir+infile)
-                Graphics(infile=src_dir+infile, outfile=src_dir + infile).cut_by_ratio()            
-        else:
-            pass
-    else:
-        print("source directory not exist!")     
-
-
-
-def git_operation():
-    '''
-    git 命令行函数，将仓库提交
-    
-    ----------
-    需要安装git命令行工具，并且添加到环境变量中
-    '''
-    os.system('git add --all')
-    os.system('git commit -m "add photos"')
-    os.system('git push origin master')
-
-if __name__ == "__main__":
-    cut_photo()        # 裁剪图片，裁剪成正方形，去中间部分
-    compress_photo()   # 压缩图片，并保存到mini_photos文件夹下
-    git_operation()    # 提交到github仓库
-    handle_photo()     # 将文件处理成json格式，存到博客仓库中
+@media screen and (max-width:600px) {
+    .instagram .thumb {
+        width: 50%;
+        padding-bottom: 50%;
+    }
+    .instagram .album li {
+        width: 100%;
+        position: relative;
+        padding-bottom: 100%;
+        text-align: center;
+    }
+    .instagram .album div.img-box{
+        margin: 0;
+        width: 90%;
+        height: 90%;
+    }
+}
